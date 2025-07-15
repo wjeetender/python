c10d3b6b55cf7a30275e167f1c77b9b0d0c399be
wjeetender <48332192+wjeetender@users.noreply.github.com>
2025-07-15 14:09:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)  # IMPORTANT: variable must be named `application`
 
 @application.route('/')
 def index():
-    return "Hello from Elastic Beanstalk!"
+    return "Hello from Elastic Beanstalk! Prod Test"
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=5000)  # expose externally
